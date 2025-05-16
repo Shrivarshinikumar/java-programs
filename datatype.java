e3dbddc0d0e28c822384c88d61b2ff531474a97b
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-05-16 13:10:44 +0530

    added datatype

---
+++ datatype.java
@@ -0,0 +1,15 @@
+class datatype{
+    public  static void main(String args[]){
+        
+        byte a = 10;
+        short b = 123;
+        int c = 12345;
+        long d = 1234567890;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(c);
+        System.out.print(b);
+        
+           }
+}
