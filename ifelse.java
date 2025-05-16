d3b815d81648d70dec3a1e126cfba823de338c4b
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-05-16 18:40:35 +0530

    added ifelse

---
+++ ifelse.java
@@ -0,0 +1,16 @@
+class ifelse{
+    public static void main(String args[]){
+
+        boolean rain = false;
+
+        if(rain){
+            System.out.print("take umbrella ");
+        }
+
+        else{
+            System.out.print("enjoy the sun");
+        }
+;
+    }
+}
+
