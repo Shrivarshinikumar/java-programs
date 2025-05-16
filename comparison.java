ce31bf20c0da1c0d4340b5ba2290aa09d9005a73
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-05-16 19:04:36 +0530

    added comparison

---
+++ comparison.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class comparison {
+    public static void main(String args[]){
+        Scanner shri = new  Scanner(System.in);
+        int num1 = shri.nextInt();
+        int num2 = shri.nextInt();
+        shri.close();
+
+        if(num1==num2){
+            System.out.print("equal");
+
+        }
+        
+        else{
+            System.out.print("not equal");
+
+    }}
+
+
+
+    }
+    
+
