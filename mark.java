31086ddf121f1f3c0df7df528626be0e2944586f
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-05-19 23:21:53 +0530

    added mark

--- mark.java
+++ mark.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class mark {
-    Scanner shri = new Scanner(System.in);
+    public static void main(String args[]){
+         Scanner shri = new Scanner(System.in);
     int mark = shri.nextInt();
 
     if(mark>35){
-        System,out.print("pass");
+        System.out.print("pass");
     }
     else{
         System.out.print("fail");
     }
     shri.close();
+    }
+   
 
     
 }
