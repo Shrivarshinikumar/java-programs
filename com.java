4950905f75183112e3eeb528bf6e5ef4ae0d4db0
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-05-19 22:53:30 +0530

    added com

---
+++ com.java
@@ -0,0 +1,15 @@
+public class com {
+    public static void main(String  args[]){
+        String a = "one";
+        String b =  "one";
+        String c = b;
+        System.out.println(a.equals(b));
+
+        String d = "two";
+        String e = new String("two");
+        System.out.println(b.equals(e));
+
+
+    }
+    
+}
